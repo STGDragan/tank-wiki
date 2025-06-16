b97b362ee5515adab96cd62be9db4c68e3652607
STGDragan <70173272+STGDragan@users.noreply.github.com>
2025-06-16 17:11:31 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
+/** 
+ * next.config.js
+ * 
+ * Remove experimental.appDir as it is no longer needed in Next.js 13.5+
+ */
+
 const nextConfig = {
+  // Add any other Next.js config here if needed
   reactStrictMode: true,
-  experimental: {
-    appDir: false
-  }
-}
+  swcMinify: true,
+};
 
 module.exports = nextConfig;
