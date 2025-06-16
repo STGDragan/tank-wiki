cbf329a834b54bdf0f5b8cde87304c4469f2676a
STGDragan <70173272+STGDragan@users.noreply.github.com>
2025-06-16 12:35:55 -0500

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,5 +4,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
+console.log("Supabase URL:", supabaseUrl)
+console.log("Supabase Anon Key:", supabaseAnonKey)
+
 // Create a single Supabase client for use in your app
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
