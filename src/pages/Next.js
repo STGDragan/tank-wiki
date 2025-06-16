0872398095f2d02a8226c33b9819233f66fe7e57
STGDragan <70173272+STGDragan@users.noreply.github.com>
2025-06-16 17:06:22 -0500

    Create Next.js

---
+++ src/pages/Next.js
@@ -0,0 +1,16 @@
+{
+  "name": "tank-wiki",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "next": "13.5.2",
+    "react": "18.3.1",
+    "react-dom": "18.3.1",
+    "@supabase/supabase-js": "^2.26.0"
+  }
+}
