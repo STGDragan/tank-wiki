b6817ec6135d23d80c755ac42433337b994ac90d
STGDragan <70173272+STGDragan@users.noreply.github.com>
2025-06-16 19:06:42 -0500

    Revert "Update _app.tsx"
    This reverts commit df60d703eb3d646f994f1d6071c8372f66b17237.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,13 @@
-// src/pages/_app.tsx
-import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// tank-wiki/src/pages/_app.tsx
 
-import { ThemeProvider } from "@/providers/ThemeProvider";
-import { AuthProvider } from "@/providers/AuthProvider";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import type { AppProps } from 'next/app';
+import { BrowserRouter } from 'react-router-dom'; // only if you're using react-router
+import "@/styles/globals.css";
 
-const queryClient = new QueryClient();
-
-export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <TooltipProvider>
-          <AuthProvider>
-            <Component {...pageProps} />
-            <Toaster />
-            <Sonner />
-          </AuthProvider>
-        </TooltipProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Component {...pageProps} />
+    </BrowserRouter>
   );
 }
