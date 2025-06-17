ad3980f655c3d270b4b06bcafc1a8c6d7d284134
STGDragan <70173272+STGDragan@users.noreply.github.com>
2025-06-16 19:34:46 -0500

    Revert "Update next.config.js"
    This reverts commit b97b362ee5515adab96cd62be9db4c68e3652607.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-/** 
- * next.config.js
- * 
- * Remove experimental.appDir as it is no longer needed in Next.js 13.5+
- */
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Add any other Next.js config here if needed
   reactStrictMode: true,
-  swcMinify: true,
-};
+  experimental: {
+    appDir: false
+  }
+}
 
 module.exports = nextConfig;
