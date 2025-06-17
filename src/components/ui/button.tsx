749adf64960b21ca1ed9292bf96bc1354abacd85
STGDragan <70173272+STGDragan@users.noreply.github.com>
2025-06-16 19:06:09 -0500

    Revert "Update button.tsx"
    This reverts commit 2b87bf43fe865425d2a8f560fe46f5a19daa09df.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,3 @@
-// /src/components/ui/button.tsx
-
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
@@ -22,7 +20,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10 p-0",
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
@@ -43,13 +41,14 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        className={cn(buttonVariants({ variant, size }), className)}
         {...props}
       />
     )
   }
 )
+
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
